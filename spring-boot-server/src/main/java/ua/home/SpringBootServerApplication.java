013354ae665b0a2598b1637d018b37fba2227935
yurasheremeta <sheremeta.yura@gmail.com>
2018-08-06 12:11:10 +0300

    init repository

---
+++ spring-boot-server/src/main/java/ua/home/SpringBootServerApplication.java
@@ -0,0 +1,12 @@
+package ua.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServerApplication.class, args);
+	}
+}
